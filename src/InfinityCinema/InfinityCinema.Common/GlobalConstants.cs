1fda353da29380ef6b64a28f42ccb871745f66fb
Radoslav Radev <88380154+calisthenicsGuy@users.noreply.github.com>
2022-08-27 11:46:33 +0300

    design updated

--- src/InfinityCinema/InfinityCinema.Common/GlobalConstants.cs
+++ src/InfinityCinema/InfinityCinema.Common/GlobalConstants.cs
@@ -4,6 +4,10 @@
     {
         public const string SystemName = "InfinityCinema";
 
+        public const string EmailAdress = @"radoslav.radev05g@gmail.com";
+
+        public const string TelephoneNumber = "+359 87 747 5376";
+
         public const string AdministratorRoleName = "Administrator";
     }
 }
---
+++ src/InfinityCinema/Services/InfinityCinema.Services.Data/HomeService/Models/LayoutViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace InfinityCinema.Services.Data.HomeService.Models
+{
+    using InfinityCinema.Common;
+    using InfinityCinema.Services.Data.MoviesService.Models;
+
+    public class LayoutViewModel
+    {
+        public LayoutViewModel()
+        {
+            //this.MoviesQueryModel = new AllMoviesQueryModel();
+            this.ProjectName = GlobalConstants.SystemName;
+            this.EmailAdress = GlobalConstants.EmailAdress;
+            this.TelephoneNumber = GlobalConstants.TelephoneNumber;
+        }
+
+        // public AllMoviesQueryModel MoviesQueryModel { get; set; }
+
+        public string ProjectName { get; set; }
+
+        public string EmailAdress { get; set; }
+
+        public string TelephoneNumber { get; set; }
+    }
+}
